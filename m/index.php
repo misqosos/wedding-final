24d4241a9d385254a908873bd514584e711906ad
misqosos <michaldrotar14@gmail.com>
2024-05-23 08:01:39 +0200

    prerabka

--- m/index.php
+++ m/index.php
@@ -73,7 +73,7 @@ include("../access/access.php");
   <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title><?php if($access) { echo "Wooding"; } else { echo "Gate"; } ?></title>
+    <title><?php if($access) { echo "Stakčínska svadbička"; } else { echo "Ahoj"; } ?></title>
     <base href="/">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="heart.ico">
