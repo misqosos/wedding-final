7c106ed2713672bc468e6d1b94cebf5a5994afe2
misqosos <michaldrotar14@gmail.com>
2024-07-17 15:13:17 +0200

    cas

--- pages/home-page/countdown/countdown.js
+++ pages/home-page/countdown/countdown.js
@@ -15,7 +15,7 @@ this.weddingDate.getMinutes() == 0)) ?
 new Date(this.now.getFullYear(), this.now.getMonth(), this.weddingDate.getDate(), this.weddingDate.getHours(), this.weddingDate.getMinutes(), this.weddingDate.getSeconds(), this.weddingDate.getMilliseconds()) :
 (this.weddingDate.getDate() >= this.now.getDate() ?
 new Date(this.now.getFullYear(), this.now.getMonth() + 1, this.weddingDate.getDate(), this.weddingDate.getHours(), this.weddingDate.getMinutes(), this.weddingDate.getSeconds(), this.weddingDate.getMilliseconds()) :
-new Date(this.now.getFullYear(), this.now.getMonth() + 1, this.weddingDate.getDate(), this.weddingDate.getHours()+1, this.weddingDate.getMinutes(), this.weddingDate.getSeconds(), this.weddingDate.getMilliseconds()));
+new Date(this.now.getFullYear(), this.now.getMonth() + 1, this.weddingDate.getDate(), this.weddingDate.getHours(), this.weddingDate.getMinutes(), this.weddingDate.getSeconds(), this.weddingDate.getMilliseconds()));
 
 monthsLeft = this.weddingDate.getMonth() - this.nextMonthCountdown.getMonth();
 daysLeft;
