d15df1f72c20b611bd3437657cbe30b882e48743
misqosos <michaldrotar14@gmail.com>
2024-08-16 10:24:59 +0200

    ccas

--- pages/menu/logistics/logistics-wo-css.php
+++ pages/menu/logistics/logistics-wo-css.php
@@ -3,9 +3,9 @@
 
     <div class="main-title">Doprava</div>
     <p class="info-text">
-        Doprava je zabezpečená autobusom z Nižnej Myšle. Zastavíme sa po vás aj vo Vyšnej Myšli na "hušim tancu".
+        Doprava je zabezpečená autobusom z Nižnej Myšle od horného ihriska.
         <br>
-        Odchod z Nižnej Myšle je o 12:30 (Z Vyšnej teda cca 12:45)
+        Odchod je o 12:00
         <br>
         <i class="fa fa-bus" aria-hidden="true"></i>
         <br>Tešime sa.
